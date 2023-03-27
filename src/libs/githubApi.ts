a0fbbb75525e339939543e6692982945fbbc10cf
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-03-27 18:55:03 -0500

    updating github api

--- src/libs/githubApi.ts
+++ src/libs/githubApi.ts
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/",
-  headers: {
-    Authorization:
-      "Bearer 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",
-  },
 });

3572af7619ac2d580b9df090bc8b5e655473cbc5
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-03-27 19:34:35 -0500

    chore: removing key

--- src/libs/githubApi.ts
+++ src/libs/githubApi.ts
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/",
-  headers: {
-    Authorization:
-      "Bearer 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",
-  },
 });
